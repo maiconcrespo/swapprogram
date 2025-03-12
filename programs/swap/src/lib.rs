f4fee5d6a8b1b2db3bd60520495878e215553772
Maicon Crespo <maiconviniciuscrespo@gmail.com>
2025-03-12 23:09:26 +0100

    First Commit

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("49zo17MxwTRGrAUt1Nj7xBLPVQ6u8QaKfGPmS5rCeVak");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
